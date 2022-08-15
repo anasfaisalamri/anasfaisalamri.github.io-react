1bae3074c0d5e8065bd91aae3e827d8e380e26d8
anasfaisalamri <anasfasialamri@gmail.com>
2022-08-15 20:46:25 +0700

    'add file logo'

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "animate.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+AOS.init({
+  startEvent: "load",
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
