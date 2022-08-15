ee21ec76264dd3a8b8abe874eb2d4bbc2f482e4b
anasfaisalamri <anasfasialamri@gmail.com>
2022-08-15 20:39:45 +0700

    add footer components

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className="bg-dark text-center text-white pb-5">
+        <p>Copyright &copy; Anas Faisal Amri 2022</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
