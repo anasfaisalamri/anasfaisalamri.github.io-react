d9e678efaefbc26567a2b6a7de266c4ff032f065
anasfaisalamri <anasfasialamri@gmail.com>
2022-08-23 17:52:38 +0700

    resource icons & image for components - About, Contact, Portfolio

---
+++ src/components/IconAbout.js
@@ -0,0 +1,14 @@
+const iconAbout = [
+  require("../assets/logo/html.png"),
+  require("../assets/logo/css.png"),
+  require("../assets/logo/bootstrap.png"),
+  require("../assets/logo/javascript.png"),
+  require("../assets/logo/reactjs.png"),
+  require("../assets/logo/github.png"),
+  require("../assets/logo/gitbash.png"),
+  require("../assets/logo/vscode.png"),
+  require("../assets/logo/photoshop.png"),
+  require("../assets/logo/illustrator.png"),
+];
+
+export default iconAbout;
